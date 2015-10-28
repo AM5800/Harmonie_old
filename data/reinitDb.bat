c98aeebfcfa6605cfbae409e265257bfdbe79bd2
am5800 <am5800@yandex.ru>
2015-10-29 04:02:49 +0800

    Fixes #1 Scripts: more information about errors

--- data/reinitDb.bat
+++ data/reinitDb.bat
@@ -1,18 +1,27 @@
+@echo off
+
 del db\Harmonie.db
-if exist db\Harmonie.db goto :fail
+if exist db\Harmonie.db goto :cantDelete
 
 for /f %%d in ('dir /b Texts\') do (
 	for /f %%f in ('dir /b Texts\%%d\hs') do (
+        echo Processing %%d\%%~nf...
 		call runPart.bat %%d %%~nf
-		if errorlevel 1 goto :fail
+		if errorlevel 1 goto :partError
 	)
 )
 
 copy db\Harmonie.db ..\harmonie\src\main\assets\Harmonie.db
-goto :success
+echo Database created successfully
+goto :end
 
-:fail
-echo "Something went wrong"
+:cantDelete
+eche Error deleting database
+goto :end
 
-:success
+:partError
+echo Error processing part
+goto :end
+
+:end
 
