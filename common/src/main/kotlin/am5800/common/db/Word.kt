fa5f1ad4007c2b3cb150bcbb4e2b28139c982cfe
am5800 <AM5800@yandex.ru>
2016-03-27 17:06:47 +0200

    Word::toString

--- common/src/main/kotlin/am5800/common/db/Word.kt
+++ common/src/main/kotlin/am5800/common/db/Word.kt
@@ -1,6 +1,7 @@
 package am5800.common.db
 
 import am5800.common.Language
+import am5800.common.code
 
 open class Word(val language: Language, val lemma: String) {
   override fun equals(other: Any?): Boolean {
@@ -19,5 +20,9 @@ open class Word(val language: Language, val lemma: String) {
     var result = language.hashCode()
     result += 31 * result + lemma.hashCode()
     return result
+  }
+
+  override fun toString(): String {
+    return "${language.code()}: $lemma"
   }
 }
