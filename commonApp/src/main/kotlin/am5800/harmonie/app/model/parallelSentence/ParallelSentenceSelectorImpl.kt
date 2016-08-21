feca1e08fb2e60c8897e152ee8f8eb41b6daeccc
am5800 <am5800@yandex.ru>
2016-08-21 12:51:34 +0200

    Avoid learning new if repetition queue is too big

--- commonApp/src/main/kotlin/am5800/harmonie/app/model/parallelSentence/ParallelSentenceSelectorImpl.kt
+++ commonApp/src/main/kotlin/am5800/harmonie/app/model/parallelSentence/ParallelSentenceSelectorImpl.kt
@@ -5,6 +5,7 @@ import am5800.common.Lemma
 import am5800.common.Sentence
 import am5800.common.utils.EnumerableDistribution
 import am5800.common.utils.functions.random
+import am5800.common.utils.functions.randomOrNull
 import am5800.harmonie.app.model.DebugOptions
 import am5800.harmonie.app.model.flow.LemmasOrderer
 import am5800.harmonie.app.model.languageCompetence.LanguageCompetence
@@ -34,6 +35,7 @@ class ParallelSentenceSelectorImpl(private val repetitionService: LemmaRepetitio
   override fun selectSentenceToShow(learnLanguage: Language, languageCompetence: List<LanguageCompetence>): SentenceAndTranslation? {
     val now = DateTime.now()
     val scheduledLemma = repetitionService.getNextScheduledLemma(learnLanguage, now)
+    val repetitionQueueSize = repetitionService.countOnDueLemmas(learnLanguage, now)
 
     val attemptedLemmas = repetitionService.getAttemptedLemmas(learnLanguage)
     val allLemmas = sentenceAndLemmasProvider.getAllLemmasSorted(learnLanguage)
@@ -41,49 +43,58 @@ class ParallelSentenceSelectorImpl(private val repetitionService: LemmaRepetitio
     val canLearnNewLemma = allLemmas.map { it }.minus(attemptedLemmas).any()
     val canRepeatRandomLemma = attemptedLemmas.any()
     val canRepeatScheduledLemma = scheduledLemma != null
+    val avoidLearningNew = repetitionQueueSize > 20
 
     val task =
-        if (canRepeatScheduledLemma && canLearnNewLemma) nextTaskDistribution.get(debugOptions.random)
-        else if (!canRepeatScheduledLemma && canLearnNewLemma) NextTask.LearnNewLemma
+        if (canRepeatScheduledLemma && canLearnNewLemma && !avoidLearningNew) nextTaskDistribution.get(debugOptions.random)
+        else if (!canRepeatScheduledLemma && canLearnNewLemma && !avoidLearningNew) NextTask.LearnNewLemma
+        else if (avoidLearningNew && canRepeatScheduledLemma) NextTask.Repeat
         else if (!canRepeatScheduledLemma && !canLearnNewLemma && canRepeatRandomLemma) NextTask.RepeatRandom
-        else throw Exception("Unsupported state")
-
+        else throw Exception("Unsupported state: $canLearnNewLemma, $canRepeatRandomLemma, $canRepeatScheduledLemma, $avoidLearningNew")
 
     when (task) {
       NextTask.LearnNewLemma -> return learnNewLemma(languageCompetence, allLemmas, attemptedLemmas)
-      NextTask.Repeat -> return selectSentence(languageCompetence, scheduledLemma!!)
-      NextTask.RepeatRandom -> return repeatRandomLemma(attemptedLemmas, languageCompetence)
+      NextTask.Repeat -> return selectSentence(languageCompetence, scheduledLemma!!, avoidLearningNew)
+      NextTask.RepeatRandom -> return repeatRandomLemma(attemptedLemmas, languageCompetence, avoidLearningNew)
     }
   }
 
-  private fun repeatRandomLemma(attemptedLemmas: List<Lemma>, competence: List<LanguageCompetence>): SentenceAndTranslation? {
+  private fun repeatRandomLemma(attemptedLemmas: List<Lemma>, competence: List<LanguageCompetence>, avoidLearningNew: Boolean): SentenceAndTranslation? {
     val lemma = attemptedLemmas.random(debugOptions.random)
-    return selectSentence(competence, lemma)
+    return selectSentence(competence, lemma, avoidLearningNew)
   }
 
   private fun learnNewLemma(competence: List<LanguageCompetence>, allLemmas: List<Lemma>, attemptedLemmas: List<Lemma>): SentenceAndTranslation? {
     val attemptedLemmasSet = attemptedLemmas.toHashSet()
     val lemma = lemmasOrderer.reorder(allLemmas.filter { !attemptedLemmasSet.contains(it) }).first()
-    return selectSentence(competence, lemma)
+    return selectSentence(competence, lemma, false)
   }
 
-  private fun selectSentence(languageCompetence: List<LanguageCompetence>, lemma: Lemma): SentenceAndTranslation? {
+  private fun selectSentence(languageCompetence: List<LanguageCompetence>, lemma: Lemma, avoidLearningNew: Boolean): SentenceAndTranslation? {
     val sentencesAndTranslation = sentenceAndLemmasProvider.getEasiestSentencesWith(lemma, languageCompetence, 50)
     val sentences = sentencesAndTranslation.map { it.sentence }
     val scores = sentenceScoreStorage.getScores(sentences)
-    val result = select(scores) ?: return null
+    val result = select(scores, avoidLearningNew) ?: return null
 
     return sentencesAndTranslation[sentences.indexOf(result)]
   }
 
-  private fun select(sentences: List<Pair<Sentence, SentenceScore?>>): Sentence? {
+  private fun select(sentences: List<Pair<Sentence, SentenceScore?>>, avoidLearningNew: Boolean): Sentence? {
     if (sentences.isEmpty()) return null
+
+    if (avoidLearningNew) {
+      val wellKnownSentences = sentences
+          .filter { it.second == SentenceScore.Clear || it.second == SentenceScore.Uncertain }
+          .map { it.first }
+      val wellKnownSentence = wellKnownSentences.randomOrNull(debugOptions.random)
+      if (wellKnownSentence != null) return wellKnownSentence
+    }
 
     val firstUnknown = sentences.firstOrNull { it.second == null }
     if (firstUnknown != null) return firstUnknown.first
 
-    val firstNonBlackout = sentences.firstOrNull { it.second != SentenceScore.Unclear } ?: return sentences.first().first
+    val result = sentences.filter { it.second != SentenceScore.Unclear }.randomOrNull(debugOptions.random) ?: return sentences.first().first
 
-    return firstNonBlackout.first
+    return result.first
   }
 }
